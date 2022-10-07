1469f764dd4b0a1c8ed918da577adbff6c2d32c4
Yahya Hafez <yhafez3@gmail.com>
2022-10-07 00:48:12 -0500

    feat: add tests

--- src/__tests__/Input.tests.tsx
+++
@@ -1,5 +0,0 @@
-// Test input component
-
-import { render, screen } from '@testing-library/react'
-import { ITest, TComponent, TProps, renderWithRouterAndStore } from './App.test'
-import Input from '../components/Input'
